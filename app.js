90ba82d7301ec421c0a875f65db1d621cb8d8a15
IVainqueur <ishimvainqueur@gmail.com>
2022-06-21 10:53:19 +0200

    Pushing to a dev branch

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const {_pick, _remove, arr_remove} = require('./oneliners')
+const nocorsRoute = require('./routes/nocors')
+const emailRoute = require('./routes/smtpEmail')
+
+
+require('dotenv').config()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.use('/nocors', nocorsRoute)
+app.use('/sendMail', emailRoute)
+
+app.use('*', (req, res)=>{
+    console.log("[log] Undocumented route")
+    res.send({code: "#Undocumented", message: "Nothing on this route yet"})
+})
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log("[log]: Server is up at PORT ", process.env.PORT)
+})
