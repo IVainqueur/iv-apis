11cbc8a2b032747ac0cb78ba4f24aa0fbf873448
IVainqueur <ishimvainqueur@gmail.com>
2023-03-20 18:15:22 +0200

    feat: added y2mate route

---
+++ routes/y2mate.js
@@ -0,0 +1,42 @@
+const app = require('express').Router()
+const axios = require('axios');
+
+app.get('/versions', (req, res) => {
+    const { id } = req.query;
+    if (!id) return res.status(400).send("No ID provided");
+    axios.post(`https://www.y2mate.com/mates/analyzeV2/ajax`, {
+        Headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: {
+            k_query: `https://www.youtube.com/watch?v=${id}`,
+        }
+    }).then(({ data }) => {
+        res.send(data)
+    }).catch((e) => {
+        res.status(500).send(e.message)
+    })
+})
+
+app.get('/download', (req, res) => {
+    const { y2mate_id, id } = req.query;
+    if (!y2mate_id || !id) return res.status(400).send("No id and/or y2mate_id provided");
+
+    axios.post(`https://www.y2mate.com/mates/convertV2/ajax`, {
+        Headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: {
+            vid: id,
+            k: y2mate_id,
+        }
+    }).then(({ data }) => {
+        res.send(data)
+    }
+    ).catch((e) => {
+        res.status(500).send(e.message)
+    }
+    )
+})
+
+module.exports = app
