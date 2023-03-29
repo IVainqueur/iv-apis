2e8c6841a2dbda3002a5dbb8b6dfc3fbdea57e0c
IVainqueur <ishimvainqueur@gmail.com>
2023-03-29 14:31:21 +0200

    [feat] the zip_name can be specified in the request

--- routes/zipped.js
+++ routes/zipped.js
@@ -8,13 +8,14 @@ const uuid = require('short-uuid').generate;
 const JSZip = require('jszip');
 
 app.post('/zip', async (req, res) => {
-    const { items } = req.body;
+    const { items, zip_name } = req.body;
 
     if (!items) return res.status(400).send("No items provided")
     if (!Array.isArray(items)) return res.status(400).send("Items must be an array")
     if (items.length < 1) return res.status(400).send("Items array must have atleast one item")
 
     const folderName = uuid();
+    const zipName = zip_name || folderName;
     fs.mkdirSync(path.join(__dirname, `../tmp/${folderName}`), { recursive: true })
 
     const zip = new JSZip();
@@ -29,7 +30,7 @@ app.post('/zip', async (req, res) => {
     }
     console.log('[log] the items array in the body -> ', items)
 
-    const zipDir = path.join(__dirname, `../tmp/${folderName}/${folderName}.zip`)
+    const zipDir = path.join(__dirname, `../tmp/${folderName}/${zipName}.zip`)
     zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
         .pipe(fs.createWriteStream(zipDir))
         .on('finish', function () {
