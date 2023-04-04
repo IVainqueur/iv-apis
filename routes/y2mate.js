60bbda785f70a8a3a560c820f1971293faa3996d
IVainqueur <ishimvainqueur@gmail.com>
2023-04-04 19:45:05 +0200

    [feat] fine tuning the y2mate and zipped apis

--- routes/y2mate.js
+++ routes/y2mate.js
@@ -1,6 +1,16 @@
 const app = require('express').Router()
 const axios = require('axios');
 
+const QUALITIES = [
+    "mp3",
+    "144p",
+    "240p",
+    "360p",
+    "480p",
+    "720p",
+    "1080p",
+]
+
 app.get('/versions', (req, res) => {
     const { link } = req.query;
     console.log("[log] Versions requested for Link: " + `\x1B[33m\x1B[1m${link}\x1B[0m`)
@@ -38,17 +48,105 @@ app.get('/downloadlink', (req, res) => {
             data: `vid=${encodeURIComponent(id)}&k=${encodeURIComponent(y2mate_id.split(" ").join('+'))}`,
         }
     )
-    .then(({ data }) => {
-        if(data.status !== "ok") throw new Error('Error while getting download link')
-        if(download && data.status === "ok") {
-            return res.redirect(data.dlink)
-        }
-        res.send(data)
-    }
-    ).catch((e) => {
+        .then(({ data }) => {
+            if (data.status !== "ok") throw new Error('Error while getting download link')
+            if (download && data.status === "ok") {
+                return res.redirect(data.dlink)
+            }
+            res.send(data)
+        })
+        .catch((e) => {
+            res.status(500).send(e.message)
+        })
+})
+
+app.get("/downloadplaylist", async (req, res) => {
+    let { link, quality } = req.query;
+
+    if (!link) return res.status(400).send("No Link provided");
+    if (!QUALITIES.includes(quality)) quality = "mp3";
+
+    try {
+        const playlist = await axios.request({
+            url: `/y2mate/versions?link=${link}`,
+            method: 'GET',
+        })
+
+        if (playlist.data.status != "ok") throw new Error("Error while getting playlist")
+        if (playlist.data.page !== "playlist") throw new Error("Not a playlist")
+
+        const zipName = playlist.data.title;
+        const videos = playlist.data.vitems.map(item => ({ name: item.t, id: item.v }));
+
+        await (new Promise((resolve, reject) => {
+            let done = 0;
+            videos.forEach((video, index) => {
+                axios.request({
+                    url: `/y2mate/versions?link=https://youtube.com/watch?v=${video.id}`,
+                    method: 'GET',
+                })
+                    .then(({ data }) => {
+                        if (data.status !== "ok") throw new Error('Error while getting download link')
+                        const { mp4: mp4s, mp3: mp3s } = data.links;
+
+                        if (quality === "mp3") {
+                            const k = Object.values(mp3s).find(el => el.f === "mp3");
+                            if (!k) throw new Error("No mp3 found")
+                            return axios.request({
+                                url: `/y2mate/downloadlink?y2mate_id=${k.k}&id=${video.id}`,
+                                method: 'GET'
+                            })
+                        }
+                        let k = ""
+                        for (const qual of Object.values(mp4s)) {
+                            if (qual.q === quality) {
+                                k = qual.k
+                                break;
+                            }
+                        }
+                        if (k === "") k = Object.values(mp4s)[0].k;
+                        return axios.request({
+                            url: `/y2mate/downloadlink?y2mate_id=${k}&id=${video.id}`,
+                            method: 'GET'
+                        })
+
+                    })
+                    .then(({ data }) => {
+                        if (data.status !== "ok") throw new Error('Error while getting download link')
+                        videos[index].url = data.dlink
+                        videos[index].name += "" + (quality === "mp3" ? ".mp3" : ".mp4")
+                    })
+                    .catch((e) => {
+                        console.log("ERROR: " + e.message)
+                        console.log('Failed to get download link for video: ' + video.name + ' (' + video.id + ')')
+                    })
+                    .finally(() => {
+                        done++;
+                        if (done === videos.length) resolve();
+                    })
+
+
+            })
+        }))
+
+
+
+        axios.request({
+            url: "/zipped/zip",
+            method: "POST",
+            data: {
+                zipName,
+                items: videos
+            }
+        })
+            .then(({ data }) => {
+                res.redirect(data)
+            })
+
+
+    } catch (e) {
         res.status(500).send(e.message)
     }
-    )
 })
 
 
--- routes/zipped.js
+++ routes/zipped.js
@@ -9,6 +9,7 @@ const JSZip = require('jszip');
 
 app.post('/zip', async (req, res) => {
     const { items, zip_name } = req.body;
+    const { download } = req.query;
 
     if (!items) return res.status(400).send("No items provided")
     if (!Array.isArray(items)) return res.status(400).send("Items must be an array")
@@ -23,10 +24,14 @@ app.post('/zip', async (req, res) => {
     for (const item of items) {
         if (!item.url.startsWith("https://") && !item.url.startsWith("http://")) continue;
 
-        const response = await axios.get(item.url, { responseType: 'arraybuffer' })
-        zip.file(item.name, response.data)
+        try {
+            const response = await axios.get(item.url, { responseType: 'arraybuffer' })
+            zip.file(item.name || uuid(), response.data)
 
-        console.log("[log] got item: " + `\x1B[33m\x1B[1m${item}\x1B[0m`)
+            console.log("[log] got item: " + `\x1B[33m\x1B[1m${item.name}\x1B[0m`)
+        } catch (e) {
+            console.log("[log] error while getting item: " + `\x1B[33m\x1B[1m${item.name}\x1B[0m`)
+        }
     }
     console.log('[log] the items array in the body -> ', items)
 
@@ -35,11 +40,17 @@ app.post('/zip', async (req, res) => {
         .pipe(fs.createWriteStream(zipDir))
         .on('finish', function () {
             console.log("[log] zip file generated")
-            res.redirect(`/download/${zipName}.zip`)
+
+            if (download) res.redirect(`/zipped/download/${folderName}/${zipName}.zip`)
+            else res.send(`/zipped/download/${folderName}/${zipName}.zip`)
 
             setTimeout(() => {
-                if (!fs.existsSync(`../tmp/${folderName}`)) return
-                fs.rmSync(path.join(__dirname, `../tmp/${folderName}`), { recursive: true })
+                try {
+                    if (!fs.existsSync(`../tmp/${folderName}`)) return
+                    fs.rmSync(path.join(__dirname, `../tmp/${folderName}`), { recursive: true, force: true })
+                } catch (e) {
+
+                }
             }, 1000 * 60 * 10)
 
         });
@@ -58,7 +69,11 @@ app.get('/download/:folder/:file', (req, res) => {
     filestream.pipe(res);
 
     filestream.on('end', () => {
-        fs.rmSync(path.join(__dirname, `../tmp/${folder}`), { recursive: true })
+        try {
+            fs.rmSync(path.join(__dirname, `../tmp/${folder}`), { recursive: true, force: true })
+        } catch (e) {
+
+        }
     })
 })
 
