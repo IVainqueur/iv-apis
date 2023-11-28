5ae237af2fcc9ef06abe8fa09b398c190ffe2c76
ivainqueur <ishimvainqueur@gmail.com>
2023-11-28 13:23:15 +0200

    Add User-Agent header and set timeout and withCredentials options

--- routes/nocors.js
+++ routes/nocors.js
@@ -43,7 +43,14 @@ app.all("/_/*", (req, res) => {
       url: url,
       method: req.method,
       json: req.body,
-      headers: { Authorization: req.header("Authorization") },
+      headers: {
+        Authorization: req.header("Authorization"),
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+      },
+      timeout: 10000,
+      withCredentials: true,
+      
     },
     function (error, response, body) {
       if (error) {
